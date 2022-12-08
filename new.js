91b076c64c7e3e99ac9800231dfd7f95f407eb1d
arpitkarnak <arpitkarnak@users.noreply.github.com>
2022-12-08 18:24:33 +0530

    add button

---
+++ new.js
@@ -0,0 +1,4 @@
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const input1 = document.querySelector("#input1");
+const output = document.querySelector("#output");
